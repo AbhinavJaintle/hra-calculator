bdeb4d38e306533eda8b300f75ff39a46ede5d45
AbhinavJaintle <2020kucp1018@iiitkota.ac.in>
2022-06-15 01:54:43 +0530

    final changes

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hra.wsgi --log-file -
--- hra/settings.py
+++ hra/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bh8e7+!84d_vph(vsji%3^*49wk8*&r7sp)52r%%wivy!i^q*9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
