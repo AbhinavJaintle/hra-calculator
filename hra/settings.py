e4450e2713454e2c0fe14e10afe0126c944706d8
AbhinavJaintle <2020kucp1018@iiitkota.ac.in>
2022-06-15 02:01:56 +0530

    final changes

--- hra/settings.py
+++ hra/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bh8e7+!84d_vph(vsji%3^*49wk8*&r7sp)52r%%wivy!i^q*9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
+ALLOWED_HOSTS = ['127.0.0.1','calculate-hra.herokuapp.com']
 
 
 # Application definition
